b0e403a60add7a71f1ddeda2a534d590e83fe20e
daithi <daithi@daithi-laptop>
2013-03-22 00:57:38 +0000

    set user paramter

--- class-wp-mvc-admin-bar.php
+++ class-wp-mvc-admin-bar.php
@@ -16,9 +16,9 @@
  * php < 5.3.0
  * 
  * Abstract methods, called in this order:
- *  - add_menus();
+ *  - initialize();		admin-bar.php:34
+ *  - add_menus();		admin-bar.php:35
  *  - render();
- *  - initialize();
  */
 class WP_MVC_Admin_Bar extends WP_Extend{
 
@@ -26,15 +26,33 @@ class WP_MVC_Admin_Bar extends WP_Extend{
 	 * @var WP_Admin_Bar
 	 */
 	private $wp_admin_bar;
+	/**
+	 * The current logged in user details. Defined in @link initialize()
+	 * @var stdClass
+	 */
+	public $user;
 
 	/**
-	 * Hooks, actions and filters. @see @link initialize() for construct flow.
+	 * Extend wp class using WP_Extender::extend_filter()
 	 * @uses WP_Extender
 	 */
 	function __construct( $extend=false ){
 
 		//extend the bar class
 		$this->extend_filter('wp_admin_bar_class', __CLASS__ );
+	}
+
+	/**
+	 * Magic Method. Catch unkown methods and try parent.
+	 * If method not found pass request on to parent to check $this->parent
+	 * @param  [type] $method The method
+	 * @param  [type] $args   Array of arguments
+	 * @return mixed returns PARENT::parent->$method( $args ) if method is found or
+	 * throws an exception if none
+	 */
+	function __call($method, $args){
+		if(!method_exists($this, $method))
+			return parent::__call($method, $args);
 	}
 
 	function debug($step=1){
@@ -47,7 +65,7 @@ class WP_MVC_Admin_Bar extends WP_Extend{
 	 * Loads actions and filters for the admin_menu
 	 */
 	public function add_menus(){
-		$this->debug();
+		return $this->parent->add_menus();
 	}
 
 	/**
@@ -56,7 +74,31 @@ class WP_MVC_Admin_Bar extends WP_Extend{
 	 * @return string [description]
 	 */
 	public function initialize(){
-		$this->debug();
+
+		/**
+		 * set user parameter
+		 */
+		$this->user = new stdClass;
+
+		if ( is_user_logged_in() ) {
+			/* Populate settings we need for the menu based on the current user. */
+			$this->user->blogs = get_blogs_of_user( get_current_user_id() );
+			if ( is_multisite() ) {
+				$this->user->active_blog = get_active_blog_for_user( get_current_user_id() );
+				$this->user->domain = empty( $this->user->active_blog ) ? user_admin_url() : trailingslashit( get_home_url( $this->user->active_blog->blog_id ) );
+				$this->user->account_domain = $this->user->domain;
+			} else {
+				$this->user->active_blog = $this->user->blogs[get_current_blog_id()];
+				$this->user->domain = trailingslashit( home_url() );
+				$this->user->account_domain = $this->user->domain;
+			}
+		} //end set user parameter
+
+		/**
+		 * Actions hooks and filters
+		 */
+		;
+		//end Actions hooks and filters
 	}
 
 	/**
@@ -65,7 +107,7 @@ class WP_MVC_Admin_Bar extends WP_Extend{
 	 * @return [type] [description]
 	 */
 	public function render(){
-		$this->debug();
+		//return $this->parent->render();
 	}
 }
 
@@ -80,6 +122,14 @@ class WP_Extend{
 	public $class_name;
 	/** @var string The parent class anme */
 	public $parent = false;
+
+	function __call($method, $args){
+		if(method_exists($this->parent, $method)){
+			return call_user_func_array(array(&$this->parent, $method), $args);
+		}
+		throw new Exception("Method => " . __CLASS__ . ":$method not found");
+		
+	}
 
 	/**
 	 * Set the child class name using a filter.
